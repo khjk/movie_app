6ee26e5780ce652b5f8941ddee6604c4e4d5a937
hyojungkang <begywjd@yonsei.ac.kr>
2020-06-17 01:07:50 +0900

    add movieApi using axios

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import axios from "axios";
+import Movie from "./Movie"
+//클래스 컴포넌트 (React컴포넌트로부터 확장한 App컴포넌트)
+class App extends React.Component{
+  state  = {
+    isLoading: true,
+    movies : []
+  };
+  getMovies = async() => { //data.data.movies -> movies
+    const {
+      data: {
+         data : {movies}
+      }
+    } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
+    this.setState({ movies, isLoading:false }) //movies : movies (state 수정)
+  }
+  componentDidMount(){ //render하면 호출되는 life cycle method
+    this.getMovies();
+  }
+  render(){
+    const {isLoading, movies} = this.state;
+    return <section class="container">
+      {isLoading ? (
+          <div class="loader">
+            <span class="loader_text">Loading...</span>
+          </div> 
+       ) : (
+          <div class="movies">
+            {movies.map(movie => (
+                <Movie 
+                  key={movie.id}
+                  id={movie.id} 
+                  year={movie.year} 
+                  title={movie.title} 
+                  summary={movie.summary} 
+                  poster={movie.medium_cover_image} />
+             ))}
+          </div>
+       )}
+    </section>
+  }
 }
-
 export default App;
