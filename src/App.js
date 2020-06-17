e26520b8b8727fa970316eff51500fc2af0499ad
hyojungkang <begywjd@yonsei.ac.kr>
2020-06-17 14:11:35 +0900

    add App.css, poster, movie.js(def movie component)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import axios from "axios";
 import Movie from "./Movie"
+import "./App.css"
+
 //클래스 컴포넌트 (React컴포넌트로부터 확장한 App컴포넌트)
 class App extends React.Component{
   state  = {
@@ -20,13 +22,13 @@ class App extends React.Component{
   }
   render(){
     const {isLoading, movies} = this.state;
-    return <section class="container">
+    return <section className="container">
       {isLoading ? (
-          <div class="loader">
-            <span class="loader_text">Loading...</span>
+          <div className="loader">
+            <span className="loader_text">Loading...</span>
           </div> 
        ) : (
-          <div class="movies">
+          <div className="movies">
             {movies.map(movie => (
                 <Movie 
                   key={movie.id}
@@ -34,7 +36,8 @@ class App extends React.Component{
                   year={movie.year} 
                   title={movie.title} 
                   summary={movie.summary} 
-                  poster={movie.medium_cover_image} />
+                  poster={movie.medium_cover_image}
+                  genres={movie.genres} />
              ))}
           </div>
        )}
