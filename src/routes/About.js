a450c03592e328db14d90629d2ddc5357cf83c0d
hyojungkang <begywjd@yonsei.ac.kr>
2020-06-18 21:22:34 +0900

    fix css , detail.js

--- src/routes/About.js
+++ src/routes/About.js
@@ -5,10 +5,11 @@ function About(){
     return (
         <div className="about__container">
           <span>
-            “Freedom is the freedom to say that two plus two make four. If that is
-            granted, all else follows.”
+            "Learning something new Everyday is an appointment with me!"
           </span>
-          <span>− George Orwell, 1984</span>
+          <span>
+            Welcome to my first website using react.
+          </span>
         </div>
       );
 }
--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Detail.css";
 
 class Detail extends React.Component{
     componentDidMount(){
@@ -8,10 +9,25 @@ class Detail extends React.Component{
         }
     }
     render(){
-       const { location } = this.props;
-       if(location.state){
-            return <span>{location.state.title}</span>;
-       }else{
+       const { location : {state}} = this.props;
+       if(state){
+        console.log(state.genres);
+            return (
+            <div className="movie">
+                <img src={state.poster} alt={state.title} title={state.title}/>
+                <div className="movie_data">
+                    <h3 className="movie_title">{state.title}</h3>
+                    <h5 className="movie_year">{state.year}</h5>
+                    <ul className="movie_genres">
+                        {(state.genres).map( (genre, order) => (
+                            <li key={order} className="genres_genre">{genre}</li> 
+                        ))}
+                    </ul>
+                    <p className="movie_summary">{state.summary}</p>
+                </div>
+            </div>
+            );
+        }else{
             return null;
        }
     }
